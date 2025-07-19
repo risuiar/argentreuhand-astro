5475974aa17fb07f272e16144be22f9e937f5d91
Ricardo Vögeli <rvoegeli@gmail.com>
2025-07-19 18:14:40 +0200

    improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server", // Cambiar a modo servidor para que funcionen los endpoints
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [
     react(),
     tailwind({
